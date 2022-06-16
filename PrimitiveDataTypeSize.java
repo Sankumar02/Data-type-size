67a9066ed809a63b952930a8e202fd9da0479384
Sankumar02 <87709397+Sankumar02@users.noreply.github.com>
2022-06-16 20:36:40 +0530

    Add files via upload

---
+++ PrimitiveDataTypeSize.java
@@ -0,0 +1,33 @@
+package com.Class.org;
+
+public class PrimitiveDataTypeSize {
+
+	public static void main(String[] args) {
+		System.out.println("Size of the Primitive Data Types are:");
+        System.out.println("Size of Char DataType in Bit : "+(Character.SIZE)+ " bit.");
+	    System.out.println("Size of Long DataType in Bit : "+(Long.SIZE)+ " bit.");
+        System.out.println("Size of Int DataType in Bit : "+(Integer.SIZE)+ " bit.");
+		System.out.println("Size of Float DataType in Bit : "+(Float.SIZE)+ " bit.");
+		System.out.println("Size of Double DataType in Bit : "+(Double.SIZE)+ " bit.");
+		System.out.println("Size of Short DataType in Bit : "+(Short.SIZE)+ " bit.");
+	    System.out.println("Size of Byte DataType in Bit : "+(Byte.SIZE)+ " bit.");
+	    System.out.println("");
+		//Size of Character is 16.
+		System.out.println("Size of Char DataType in Byte : "+(Character.SIZE/8)+ " byte.");
+		//Size of Long is 64.
+		System.out.println("Size of Long DataType in Byte : "+(Long.SIZE/8)+ " byte.");
+		//Size of Integer is 32.
+		System.out.println("Size of Int DataType in Byte : "+(Integer.SIZE/8)+ " byte.");
+		//Size of Float is 32.
+		System.out.println("Size of Float DataType in Byte : "+(Float.SIZE/8)+ " byte.");
+		//Size of Double is 64.
+		System.out.println("Size of Double DataType in Byte : "+(Double.SIZE/8)+ " byte.");
+		//Size of Short is 16.
+		System.out.println("Size of Short DataType in Byte : "+(Short.SIZE/8)+ " byte.");
+		//Size of Byte is 8.
+		System.out.println("Size of Byte DataType in Byte : "+(Byte.SIZE/8)+ " byte.");
+
+	}
+
+}
+
